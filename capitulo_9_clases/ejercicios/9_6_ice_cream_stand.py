1729b3dcd5069af3f909d1216d9af85c01a889ae
JuanJesuses <juanj.rodriguez.sanchez@gmail.com>
2022-12-12 15:13:56 +0100

    "Continúo con el tema 9 hasta el ejercicio 9-6 sin hacer."

---
+++ capitulo_9_clases/ejercicios/9_6_ice_cream_stand.py
@@ -0,0 +1,48 @@
+# Un puesto de helados es un tipo específico de restaurante. Escribe una clase llamada IceCreamStand que herede
+# de la clase Restaurant que escribiste en el ejercicio 9-1 o en el 9-4. Cualquiera de las versiones de la clase
+# funcionará, sólo elige la que más te guste. Añade un atributo llamado flavours que almacene una lista
+# de sabores de helados. Escribe un método que muestre esos sabores.
+# Crea una instancia de IceCreamStand y llama a este método.
+
+class Restaurant:
+    """Clase que modela un restaurante."""
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+
+    def describe_restaurant(self):
+        """Imprime el nombre y el tipo de cocina del restaurante."""
+        print(f"El restaurante se llama {self.restaurant_name}.")
+        print(f"El tipo de cocina es {self.cuisine_type}. Muy rica.")
+        print(f"El restaurante ha atendido a {self.number_served} clientes hoy.")
+
+    def open_restaurant(self):
+        """Imprime un mensaje diciendo que el restaurante está abierto."""
+        print(f"El restaurante {self.restaurant_name} está abierto.")
+
+    def set_number_served(self, number_served):
+        """Establece el número de clientes que han sido servidos."""
+        self.number_served = number_served
+
+    def increment_number_served(self, increment):
+        self.number_served += increment
+
+class IceCreamStand(Restaurant):
+    """Modela un tipo de restaurante dedicado a los helados."""
+    def __init__(self, restaurant_name, cuisine_type, flavours):
+        """Representa aspectos de un tipo de restaurante dedicado a los helados."""
+        super.__init__(restaurant_name, cuisine_type)
+        self.flavours = [flavours]
+
+    def show_flavours(self, sabores):
+        """Muestra una lista con los sabores de los helados."""
+        num = 1
+        for sabor in self.flavours:
+            print(f"{num} - {sabor}")
+            num += 1
+
+
+restaurante_postre = IceCreamStand('shanaya', 'árabe')
+restaurante_postre.show_flavours(['yogur', 'fresa', 'mango', 'stracciatela'])
+
---
+++ capitulo_9_clases/electric_car.py
@@ -0,0 +1,75 @@
+class Car:
+    """un simple intento de representar un coche."""
+
+    def __init__(self, make, model, year):
+        """Inicializa los atributos para describir un coche."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Devuelve un nombre descriptivo con formato ordenado."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        """Imprime una frase que muestra el kilometraje del coche."""
+        print(f"Este coche tiene {self.odometer_reading} kilómetros.")
+
+    def update_odometer(self, mileage):
+        """Establece la lectura del cuentakilométros al valor dado.
+           Rechaza el cambio si se intenta hacer retroceder el cuentakilómetros."""
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("no puedes retroceder el cuentakilómetros, hijueputa!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+    def fill_gas_tank(self, amount):
+        """Indica la cantida de combustible que tiene el coche"""
+        self.amount = amount
+        print(f"El coche tiene {self.amount} litros de gasolina.")
+
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+    def __init__(self, battery_size=75):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        """Imprime una frase acerca del alcance que proporciona esta batería."""
+        if self.battery_size == 75:
+            range = 418.43
+        elif self.battery_size == 100:
+            range = 506.94
+
+        print(f"Este coche puede circular unos {range} kilómetros con una carga completa.")
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class.
+           Then initialize attributes specific to an electric car."""
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+    def fill_gas_tank(self):
+        """Los coches eléctricos no necesitan un tanque de gasolina."""
+        print("Este coche no necesita un tanque de gasolina.")
+
+
+#coche1 = Car('Ssang Yong', 'Rodius', 2015)
+#coche1.fill_gas_tank(75)
+#my_tesla.fill_gas_tank()
+
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
